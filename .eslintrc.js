cad0d1b0e0f6431056f020ab50d62743318b4c4c
= <Jie.Liu4@budweiserapac.com>
2022-02-24 11:47:32 +0800

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   rules: {
     'no-unused-vars':'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-parsing-error': ['error', { 'x-invalid-end-tag': false }],
     'vue/singleline-html-element-content-newline': 'off',
